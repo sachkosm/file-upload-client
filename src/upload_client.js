56c4140816a93edcc26da9e55da363e415ef887e
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-09-09 11:02:01 -0400

    Create upload_client.js

---
+++ src/upload_client.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var request = require('request'); //https://github.com/request/request#custom-http-headers
+
+
+
+var r = request.post('http://localhost:3002/upload',
+  function optionalCallback(err, httpResponse, body) {
+    if (err) {
+      return console.error('upload failed:', err);
+    }
+    console.log('Upload successful!  Server responded with:', body);
+  })
+var form = r.form()
+//single file only under the same form-field name
+form.append('files', fs.createReadStream('./test-upload-files/httpRequest.md'))
