99944adb5711c9a8e92427806a2f88b3bcc0c5c4
sachkosm <39930413+sachkosm@users.noreply.github.com>
2019-09-09 11:02:35 -0400

    Create http-request.js

---
+++ src/http-request.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var request = require('request'); //https://github.com/request/request#custom-http-headers
+request.post({
+    url: 'http://localhost:3002/upload',
+    formData: {
+        //multiple files as an array - this is expected in the server middle tier
+        files: [fs.createReadStream('./test-upload-files/form_data.md'), fs.createReadStream('./test-upload-files/excel_limit.png')],
+        firstName: 'Daniel',
+        lastName: 'Bethoven',
+
+
+    }
+}, function (error, response, body) {
+    console.log(body);
+});
